f1962a2489cc64253e3a5ae944f58d1e43f00481
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-11-20 00:12:33 +0800

    exception handling

---
+++ 10. exception-handling.dart
@@ -0,0 +1,35 @@
+// Exception Handling
+
+int mustGreaterThanZero(int val) {
+  if (val <= 0) {
+    throw Exception('Value must be greater than zero.');
+  }
+
+  return val;
+}
+
+void letVerifyTheValue(var val) {
+  var valueVerification;
+
+  try {
+    valueVerification = mustGreaterThanZero(val);
+  } catch (e) {
+    print(e);
+  } finally {
+    if (valueVerification == null) {
+      print('Value is not accepted');
+    } else {
+      print('Value verified: $valueVerification');
+    }
+  }
+}
+
+void main() {
+  letVerifyTheValue(10);
+  // Value verified: 10
+  
+  
+  letVerifyTheValue(0);
+  // Exception: Value must be greater than zero.
+  // Value is not accepted
+}
