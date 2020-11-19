e8ccd8626e7d2ec190ea529370e765c5dd3a6ff3
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-11-19 19:23:36 +0800

    list, set, map

--- 4. collection.dart
+++ 4. collection.dart
@@ -1,1 +1,51 @@
 // collection [list, set, map]
+
+void main() {
+  // ====================== List ======================
+  List names = ['Arif', 'Khan', 1, 2.2];
+  List<String> names1 = ['Arif', 'Khan'];
+  List<String> names2 = const ['Arif', 'Khan'];
+
+  for (var name in names) {
+    print(name);
+  }
+
+  names[1] = 'Test';
+  names1[1] = 'Test';
+  // names2[1] = 'Test';  // not possible
+
+  var names3 = names;
+  names[0] = 'Test2';
+
+  for (var i = 0; i < names3.length; i++) {
+    print(names[i]);
+  }
+
+  var names4 = [...names]; // it will copy names list
+
+  // ====================== Set ======================
+  var set1 = {'Arif', 'Khan', 'Arif'}; // unique collection
+  print(set1.length); // 2
+
+  var set2 = <String>{};
+  Set<String> names123 = {};
+
+  print(names123.runtimeType);
+
+  // ====================== Map ======================
+  var gifts = Map();
+  gifts['first'] = 'Mango';
+  print(gifts['first']);
+
+  var person = {
+    /// key: value
+    'name': 'Arif',
+    'phone': 12345
+  };
+
+  print(person['name']); // Arif
+
+  var person2 = {2: 'Khan', 6: 34634565};
+
+  print(person2[2]); // Khan
+}
